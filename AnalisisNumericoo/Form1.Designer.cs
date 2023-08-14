8efe5dea64d594a11dd582ef3f7a51efa5360f1f
Leonardo Amerio <leoamerio14@gmail.com>
2023-08-14 20:53:22 -0300

    cambios iniciales

--- AnalisisNumericoo/Form1.Designer.cs
+++ AnalisisNumericoo/Form1.Designer.cs
@@ -29,46 +29,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.functionInput = new System.Windows.Forms.TextBox();
+            this.xiValue = new System.Windows.Forms.TextBox();
+            this.xdValue = new System.Windows.Forms.TextBox();
+            this.toleranceValue = new System.Windows.Forms.TextBox();
+            this.iterationValue = new System.Windows.Forms.TextBox();
+            this.functionLabel = new System.Windows.Forms.Label();
+            this.xiLabel = new System.Windows.Forms.Label();
+            this.xdLabel = new System.Windows.Forms.Label();
+            this.toleranceLabel = new System.Windows.Forms.Label();
+            this.iterationLabel = new System.Windows.Forms.Label();
+            this.biseccionBtn = new System.Windows.Forms.Button();
+            this.warningLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // functionInput
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.ForeColor = System.Drawing.Color.Coral;
-            this.button1.Location = new System.Drawing.Point(1, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 27);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = false;
+            this.functionInput.Location = new System.Drawing.Point(87, 43);
+            this.functionInput.Name = "functionInput";
+            this.functionInput.Size = new System.Drawing.Size(123, 20);
+            this.functionInput.TabIndex = 0;
             // 
-            // button2
+            // xiValue
             // 
-            this.button2.Location = new System.Drawing.Point(100, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.xiValue.Location = new System.Drawing.Point(301, 43);
+            this.xiValue.Name = "xiValue";
+            this.xiValue.Size = new System.Drawing.Size(115, 20);
+            this.xiValue.TabIndex = 1;
+            // 
+            // xdValue
+            // 
+            this.xdValue.Location = new System.Drawing.Point(490, 43);
+            this.xdValue.Name = "xdValue";
+            this.xdValue.Size = new System.Drawing.Size(104, 20);
+            this.xdValue.TabIndex = 2;
+            // 
+            // toleranceValue
+            // 
+            this.toleranceValue.Location = new System.Drawing.Point(137, 149);
+            this.toleranceValue.Name = "toleranceValue";
+            this.toleranceValue.Size = new System.Drawing.Size(107, 20);
+            this.toleranceValue.TabIndex = 3;
+            this.toleranceValue.TextChanged += new System.EventHandler(this.toleranceValue_TextChanged);
+            // 
+            // iterationValue
+            // 
+            this.iterationValue.Location = new System.Drawing.Point(368, 149);
+            this.iterationValue.Name = "iterationValue";
+            this.iterationValue.Size = new System.Drawing.Size(134, 20);
+            this.iterationValue.TabIndex = 4;
+            this.iterationValue.TextChanged += new System.EventHandler(this.iterationValue_TextChanged);
+            // 
+            // functionLabel
+            // 
+            this.functionLabel.Location = new System.Drawing.Point(87, 22);
+            this.functionLabel.Name = "functionLabel";
+            this.functionLabel.Size = new System.Drawing.Size(70, 16);
+            this.functionLabel.TabIndex = 5;
+            this.functionLabel.Text = "Function";
+            // 
+            // xiLabel
+            // 
+            this.xiLabel.Location = new System.Drawing.Point(301, 22);
+            this.xiLabel.Name = "xiLabel";
+            this.xiLabel.Size = new System.Drawing.Size(66, 18);
+            this.xiLabel.TabIndex = 6;
+            this.xiLabel.Text = "Xi";
+            // 
+            // xdLabel
+            // 
+            this.xdLabel.Location = new System.Drawing.Point(490, 22);
+            this.xdLabel.Name = "xdLabel";
+            this.xdLabel.Size = new System.Drawing.Size(66, 18);
+            this.xdLabel.TabIndex = 7;
+            this.xdLabel.Text = "Xd";
+            // 
+            // toleranceLabel
+            // 
+            this.toleranceLabel.Location = new System.Drawing.Point(137, 131);
+            this.toleranceLabel.Name = "toleranceLabel";
+            this.toleranceLabel.Size = new System.Drawing.Size(93, 15);
+            this.toleranceLabel.TabIndex = 8;
+            this.toleranceLabel.Text = "Tolerance";
+            // 
+            // iterationLabel
+            // 
+            this.iterationLabel.Location = new System.Drawing.Point(368, 133);
+            this.iterationLabel.Name = "iterationLabel";
+            this.iterationLabel.Size = new System.Drawing.Size(73, 13);
+            this.iterationLabel.TabIndex = 9;
+            this.iterationLabel.Text = "Iterations";
+            // 
+            // biseccionBtn
+            // 
+            this.biseccionBtn.Location = new System.Drawing.Point(167, 250);
+            this.biseccionBtn.Name = "biseccionBtn";
+            this.biseccionBtn.Size = new System.Drawing.Size(113, 41);
+            this.biseccionBtn.TabIndex = 10;
+            this.biseccionBtn.Text = "Biseccion";
+            this.biseccionBtn.UseVisualStyleBackColor = true;
+            this.biseccionBtn.Click += new System.EventHandler(this.biseccionBtn_Click);
+            // 
+            // warningLabel
+            // 
+            this.warningLabel.Location = new System.Drawing.Point(327, 250);
+            this.warningLabel.Name = "warningLabel";
+            this.warningLabel.Size = new System.Drawing.Size(136, 31);
+            this.warningLabel.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.warningLabel);
+            this.Controls.Add(this.biseccionBtn);
+            this.Controls.Add(this.iterationLabel);
+            this.Controls.Add(this.toleranceLabel);
+            this.Controls.Add(this.xdLabel);
+            this.Controls.Add(this.xiLabel);
+            this.Controls.Add(this.functionLabel);
+            this.Controls.Add(this.iterationValue);
+            this.Controls.Add(this.toleranceValue);
+            this.Controls.Add(this.xdValue);
+            this.Controls.Add(this.xiValue);
+            this.Controls.Add(this.functionInput);
+            this.HelpButton = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label warningLabel;
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button biseccionBtn;
+
+        private System.Windows.Forms.Label functionLabel;
+        private System.Windows.Forms.Label xiLabel;
+        private System.Windows.Forms.Label xdLabel;
+        private System.Windows.Forms.Label toleranceLabel;
+        private System.Windows.Forms.Label iterationLabel;
+
+        private System.Windows.Forms.TextBox toleranceValue;
+
+        private System.Windows.Forms.TextBox iterationValue;
+        
+        private System.Windows.Forms.TextBox xdValue;
+
+        private System.Windows.Forms.TextBox xiValue;
+
+        private System.Windows.Forms.TextBox functionInput;
 
         #endregion
     }
--- AnalisisNumericoo/Form1.cs
+++ AnalisisNumericoo/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Calculus;
 
 namespace AnalisisNumericoo
 {
@@ -22,9 +23,70 @@ namespace AnalisisNumericoo
             throw new System.NotImplementedException();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Biseccion(object sender, EventArgs e)
+        {
+
+            double fx, area;
+
+
+            // if (AnalizadorDeFunciones.Sintaxis("2*x+2", 'x')) //pasamos la funcion con la variable a evaluar
+            // {
+            //     fx = AnalizadorDeFunciones.EvaluaFx(2.3); 
+            //     area= AnalizadorDeFunciones.Integra(2,5,0.0003);
+            //      
+            //     Console.WriteLine("f(2.3)={0} Area={1}", fx,area);
+            //
+            // }
+            // else{
+            //     // aquí mensaje de error en sintaxis
+            // }
+        }
+
+        private void Form1_DragLeave(object sender, EventArgs e)
         {
             throw new System.NotImplementedException();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void toleranceValue_TextChanged(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void iterationValue_TextChanged(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void biseccionBtn_Click(object sender, EventArgs e)
+        {
+            if (functionInput.Text != "" && xiValue.Text != "" && xdValue.Text != "" && toleranceValue.Text != "" &&
+                iterationValue.Text != "")
+            {
+                warningLabel.Text = "";
+                Calculo AnalizadorDeFunciones = new Calculo();
+                string fx = functionInput.Text;
+                AnalizadorDeFunciones.Sintaxis(fx, 'x');
+                double xi = double.Parse(xiValue.Text);
+                double xd = double.Parse(xiValue.Text);
+                double fXi = AnalizadorDeFunciones.EvaluaFx(xi);
+                double fXd = AnalizadorDeFunciones.EvaluaFx(xd);
+                while (fXi * fXd > 0)
+                {
+                    
+                }
+            }
+        }
+        
+        
     }
 }
